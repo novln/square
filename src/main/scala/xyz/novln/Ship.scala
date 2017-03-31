01c6a5dfc06a8a6d918592808b58768937e0f75b
Thomas LE ROUX <thomas@leroux.io>
2017-03-31 10:46:42 +0200

    chore: Simplify code

--- src/main/scala/xyz/novln/Ship.scala
+++ src/main/scala/xyz/novln/Ship.scala
@@ -1,6 +1,6 @@
 package xyz.novln
 
-case class Ship(id: ShipIdentifier, crew: Set[Human] = Set.empty[Human])
+case class Ship(id: String, crew: Set[Human] = Set.empty[Human])
 
 object Ship {
 
@@ -13,20 +13,5 @@ object Ship {
     val crew = ship.crew - human
     ship.copy(crew = crew)
   }
-
-}
-
-trait ShipIdentifier {
-  def id: String
-  override def toString() = id
-}
-
-object ShipIdentifier {
-
-  def apply(id: String): Option[ShipIdentifier] = {
-    Some(ShipIdentifierImpl(id))
-  }
-
-  private case class ShipIdentifierImpl(id: String) extends ShipIdentifier
 
 }
